e7b11ae2a252ff00db1a103dcd1dd496100bb966
Tina <tchen9@stuy.edu>
2018-05-18 14:22:36 -0400

    combined database with app.py

--- app.py
+++ app.py
@@ -5,7 +5,101 @@ import sqlite3   #enable control of an sqlite database
 
 app = Flask(__name__)
 
+#-------------------------------database functions-----------------------------
+f = "timber.db"
+db = sqlite3.connect(f)
+c = db.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT NOT NULL);')
+c.execute('CREATE TABLE IF NOT EXISTS entries (username TEXT, date NUMERIC, type NUMERIC, data TEXT);')
+db.close()
 
+#adds user to user database
+#returns true if successful
+def adduser(username, password):
+    f = "timber.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    if empty_db():
+        c.execute('INSERT INTO users VALUES("%s", "%s");' %(username, password))
+        db.commit()
+        db.close()
+        return True
+    if get_password(username) is None:
+        c.execute('INSERT INTO users VALUES("%s", "%s");' %(username, password))
+        db.commit()
+        db.close()
+        return True
+    db.close()
+    return False
+
+#returns true if database is empty
+def empty_db():
+    f = "timber.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    c.execute('SELECT * FROM users;')
+    results = c.fetchall()
+    return results == []
+
+#retrieves password with given username
+def get_password(username):
+    f = "timber.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    c.execute('SELECT password FROM users WHERE username="%s";' %(username))
+    results = c.fetchall()
+    if results == []:
+        db.close()
+        return None
+    else:
+        db.close()
+        return results[0][0]
+
+#changes password of user
+#returns true if changed
+def change_password(username, password):
+    f = "timber.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    c.execute('UPDATE users SET password="%s" WHERE username="%s";' %(password, username))
+    db.commit()
+    db.close()
+    return True
+
+#adds an entry to the database
+#returns true if added
+def addentry(username, date, numtype, data):
+    f = "timber.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    if empty_db():
+        c.execute('INSERT INTO entries VALUES("%s", "%s", "%s", "%s");' %(username, date, numtype, data))
+        db.commit()
+        db.close()
+        return True
+    if get_entry(username, date) is None:
+        c.execute('INSERT INTO entries VALUES("%s", "%s", "%s", "%s");' %(username, date, numtype, data))
+        db.commit()
+        db.close()
+        return True
+    db.close()
+    return False
+
+#returns a list of the entries under a username
+def getentry(username, date):
+    f = "timber.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    c.execute('SELECT type, data FROM entries WHERE username="%s" AND date="%s";' %(username, date))
+    results = c.fetchall()
+    if results == []:
+        db.close()
+        return None
+    else:
+        db.close()
+        return results
+    
+#------------------------------------flask app--------------------------------
 @app.route('/')
 def root():
     if 'user' in session:
@@ -41,10 +135,6 @@ def home():
 def logout():
     session.pop('user')
     return redirect(url_for('login'))
-        
-
-
-
 
 if __name__ == '__main__':
     app.debug = True
